0692e3762266316fe6dae300689ac53ea1b8c41b
Daniel Bolotaro <90805042+Bolotaro@users.noreply.github.com>
2022-05-11 23:52:54 -0300

    Projeto Finalizado!

--- main.js
+++ main.js
@@ -49,13 +49,27 @@ function showBackToTopButtonOnScroll() {
 }
 
 function transformBackToTopButtonInWhiteOnFooter() {
-  if (scrollY > 2630 && innerWidth > 1024) {
+  console.log(scrollY);
+  if (scrollY > 3300 && innerWidth >= 1424) {
     backToTopButton.classList.add("white-button");
-  } else if (scrollY > 4220 && innerWidth < 1024 && innerWidth > 340) {
+  } else if (scrollY > 3250 && innerWidth < 1424 && innerWidth >= 1024) {
     backToTopButton.classList.add("white-button");
-  } else if (scrollY > 4530 && innerWidth < 340) {
+  } else if (scrollY > 4755 && innerWidth <= 1023 && innerWidth >= 815) {
     backToTopButton.classList.add("white-button");
-    console.log(scrollY);
+  } else if (scrollY > 4810 && innerWidth <= 814 && innerWidth >= 660) {
+    backToTopButton.classList.add("white-button");
+  } else if (scrollY > 4850 && innerWidth <= 659 && innerWidth >= 527) {
+    backToTopButton.classList.add("white-button");
+  } else if (scrollY > 4890 && innerWidth <= 526 && innerWidth >= 453) {
+    backToTopButton.classList.add("white-button");
+  } else if (scrollY > 4928 && innerWidth <= 452 && innerWidth >= 410) {
+    backToTopButton.classList.add("white-button");
+  } else if (scrollY > 5970 && innerWidth <= 409 && innerWidth >= 376) {
+    backToTopButton.classList.add("white-button");
+  } else if (scrollY > 5125 && innerWidth <= 375 && innerWidth >= 321) {
+    backToTopButton.classList.add("white-button");
+  } else if (scrollY > 5485 && innerWidth <= 320) {
+    backToTopButton.classList.add("white-button");
   } else {
     backToTopButton.classList.remove("white-button");
   }
@@ -76,3 +90,21 @@ ScrollReveal({
 }).reveal(
   `#home, #home img, #home .stats, #services, #services header, #services .card, #about, #about header, #about .content`
 );
+
+const swiper = new Swiper(".swiper-container", {
+  slidesPerView: 1,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  mousewheel: true,
+  keyboard: true,
+  speed: 800,
+  spaceBetween: 40,
+  loop: true,
+  breakpoints: {
+    1100: {
+      slidesPerView: 2,
+      setWrapperSize: true,
+    },
+  },
+});
